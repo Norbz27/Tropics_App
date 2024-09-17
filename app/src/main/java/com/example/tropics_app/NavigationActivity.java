058fb103f549dae9988b068de082c26bc23b469a
Norberto <bruzonnorberto@gmail.com>
2024-09-17 21:45:26 +0800

    .

--- app/src/main/java/com/example/tropics_app/NavigationActivity.java
+++ app/src/main/java/com/example/tropics_app/NavigationActivity.java
@@ -2,9 +2,11 @@ package com.example.tropics_app;
 
 import android.content.Intent;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import android.view.MenuItem;
+import android.view.Window;
 
 
 import androidx.annotation.NonNull;
@@ -12,6 +14,7 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -20,13 +23,15 @@ import com.google.android.material.navigation.NavigationView;
 
 public class NavigationActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawerLayout;
-    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Window window = getWindow();
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.darkgray));
+        }
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -64,10 +69,6 @@ public class NavigationActivity extends AppCompatActivity implements NavigationV
         } else if (id == R.id.nav_inventory) {
             getSupportFragmentManager().beginTransaction().replace(R.id.framelayout, new InventoryFragment()).commit();
         } else if (id == R.id.nav_sign_out) {
-            // SharedPreferences.Editor editor = sharedPreferences.edit();
-            // editor.clear();
-            // editor.commit();
-            // editor.apply();
             Intent intent = new Intent(this, SignInActivity.class);
             startActivity(intent);
             finish();
--- app/src/main/java/com/example/tropics_app/SignInActivity.java
+++ app/src/main/java/com/example/tropics_app/SignInActivity.java
@@ -1,14 +1,17 @@
 package com.example.tropics_app;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -19,6 +22,10 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Window window = getWindow();
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.darkgray));
+        }
 
         Button btnSignIn = findViewById(R.id.btnSignIn);
         intent = new Intent(this, NavigationActivity.class);
