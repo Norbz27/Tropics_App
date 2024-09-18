7a08bbf7b6f5ea53b4238314929cb25ec5e46015
ron <bautista.130018@surigao.sti.edu.ph>
2024-09-18 09:38:28 +0800

    try

--- app/src/main/java/com/example/tropics_app/SignInActivity.java
+++ app/src/main/java/com/example/tropics_app/SignInActivity.java
@@ -8,33 +8,79 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignInActivity extends AppCompatActivity {
     Intent intent;
+    private EditText username, password1;
+    private Button btnSignin;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        mAuth = FirebaseAuth.getInstance();
+        username = findViewById(R.id.edUsername);
+        password1 = findViewById(R.id.edPassword);
+        btnSignin = findViewById(R.id.btnSignIn);
+        intent = new Intent(this, NavigationActivity.class);
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             Window window = getWindow();
             window.setStatusBarColor(ContextCompat.getColor(this, R.color.darkgray));
         }
 
-        Button btnSignIn = findViewById(R.id.btnSignIn);
-        intent = new Intent(this, NavigationActivity.class);
-        btnSignIn.setOnClickListener(new View.OnClickListener() {
+        btnSignin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(intent);
+                String email = username.getText().toString().trim();
+                String password = password1.getText().toString().trim();
+                signInUser(email, password);
+
             }
         });
+    }
+
+    private void signInUser(String email, String password){
+
+        if(email.isEmpty()){
+            username.setError("Butangi Username");
+            username.requestFocus();
+            return;
+    }
+        if(password.isEmpty()){
+            password1.setError("Butangi Password");
+            password1.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            startActivity(intent);
+                        }
+                        else{
+                            Toast.makeText(SignInActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
     }
 }
