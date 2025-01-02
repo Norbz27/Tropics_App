373140d0cce176fa96e1afbf3b89330f63cf67b7
Norberto <bruzonnorberto@gmail.com>
2025-01-02 14:25:45 +0800

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.tropics_app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 22
-        versionName = "2.2"
+        versionCode = 23
+        versionName = "2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
